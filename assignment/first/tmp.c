c0b4b04af726099de21b020b9d2152e60628dc74
dirge <yujiawei@buaa.edu.cn>
2022-03-07 22:42:28 +0800

    the forth question solved

--- assignment/first/tmp.c
+++ assignment/first/tmp.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 
 main(){
     char str[]= "hello", str1[]= "emmmm";
-    printf("%s%s", str, str1);
+    if(strcmp(str, str1) > 0)
+        printf("1");
+    else    
+        printf("hello");
+    return;
 }
